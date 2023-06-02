8b789513ea57468e59fdd2426047ac401b5ad45f
antony-chi <antony.cp9@gmail.com>
2023-06-01 19:57:56 -0500

    export default export individual

--- src/index.js
+++ src/index.js
@@ -1,28 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Greeting, UserCard } from "./Greeting";
+import Product from "./Product"
 //---- iniciar elemento ROOT----
 //const rootElement = document.getElementById('root');//obtenemos el elemento root del html
 //ReactDOM.createRoot(rootElement)//ReacDOM se la pasamos en la funcion createRoot
 
 const root = ReactDOM.createRoot(document.getElementById("root")); //listo el elemento raiz/root inicializada
 
-function Greeting() {
-  /* const married = false;
-
-  return <h1>{married ? "estoy casado 🤐" : "no estoy casado 😔"}</h1>; //operador ternario
-  */
-  function add(x, y) {
-    return x + y;
-  }
-
-  return <h1>{add(10, 30)}</h1>;
-}
-
 root.render(
   <>
     <Greeting />
-    <Greeting />
-    <Greeting />
-    <Greeting />
+    <UserCard/>
+    <Product/>
   </>
 );
