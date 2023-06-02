83a0b81781e53c3619d25989000deff5b06567a4
antony-chi <antony.cp9@gmail.com>
2023-06-02 14:50:33 -0500

    Arrays React

--- src/index.js
+++ src/index.js
@@ -4,8 +4,33 @@ import { Posts } from "./Post";
 
 const root = ReactDOM.createRoot(document.getElementById("root")); //listo el elemento raiz/root inicializada
 
+const users = [
+  {
+    id: 1,
+    name: "Rayan Rex",
+    image: "https://robohash.org/user1",
+  },
+  {
+    id: 2,
+    name: "Bryan",
+    image: "https://robohash.org/user2",
+  },
+  {
+    id: 3,
+    name: "Javi",
+    image: "https://robohash.org/user3",
+  },
+];
+
 root.render(
   <>
-    <Posts/>
+    {users.map((user, i) => {
+      return (
+        <div key={i}>
+          <h1>{user.name}</h1>
+          <img src={user.image} />
+        </div>
+      );
+    })}
   </>
 );
