21bf75a11aec2b50a498d9f7b171889bf6861bb3
antony-chi <antony.cp9@gmail.com>
2023-06-02 13:56:10 -0500

    fetch API

--- src/index.js
+++ src/index.js
@@ -4,30 +4,16 @@ import { Greeting, UserCard } from "./Greeting";
 import Product from "./Product";
 import { Button } from "./Button";
 import { TaskCard } from "./Task";
-import {Saludar} from './Saludar'
+import { Saludar } from "./Saludar";
+import { Posts } from "./Post";
 //---- iniciar elemento ROOT----
 //const rootElement = document.getElementById('root');//obtenemos el elemento root del html
 //ReactDOM.createRoot(rootElement)//ReacDOM se la pasamos en la funcion createRoot
 
 const root = ReactDOM.createRoot(document.getElementById("root")); //listo el elemento raiz/root inicializada
 
-const handdlerChange = (event) => {
-  console.log(event.target.value)
-}
-
 root.render(
   <>
-    <TaskCard ready= {true}/>
-    <Saludar/>
-    <Button text="saludar"/>
-
-    <form onSubmit={(event)=>{
-      event.preventDefault()
-      alert("enviado")
-      }}>
-      <h1>Registro de Usuario</h1>
-      <button>Send</button>
-    </form>
-
+    <Posts/>
   </>
 );
