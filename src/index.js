c2bfd0c948839ff002c2f2485f4ae87e83219a0d
antony-chi <antony.cp9@gmail.com>
2023-06-02 14:15:38 -0500

    clear index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Posts } from "./Post";
-//---- iniciar elemento ROOT----
-//const rootElement = document.getElementById('root');//obtenemos el elemento root del html
-//ReactDOM.createRoot(rootElement)//ReacDOM se la pasamos en la funcion createRoot
 
 const root = ReactDOM.createRoot(document.getElementById("root")); //listo el elemento raiz/root inicializada
 
