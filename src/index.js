4724c2cec06c985f545f258166cc9e09375daa1a
antony-chi <antony.cp9@gmail.com>
2023-06-02 16:03:06 -0500

    hook useState

--- src/index.js
+++ src/index.js
@@ -1,36 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { Posts } from "./Post";
 
 const root = ReactDOM.createRoot(document.getElementById("root")); //listo el elemento raiz/root inicializada
 
-const users = [
-  {
-    id: 1,
-    name: "Rayan Rex",
-    image: "https://robohash.org/user1",
-  },
-  {
-    id: 2,
-    name: "Bryan",
-    image: "https://robohash.org/user2",
-  },
-  {
-    id: 3,
-    name: "Javi",
-    image: "https://robohash.org/user3",
-  },
-];
+const Counter = () => {
+  const [mensage, setMensaje ] = useState(""); //hook useState es un estado
 
+
+  return (
+    <div>
+     <input onChange={(e) => setMensaje(e.target.value)} />
+     <button onClick={() =>{
+      alert("el mensaje es: "+ mensage)
+     }}>
+      Save
+     </button>
+    </div>
+  );
+};
 root.render(
   <>
-    {users.map((user, i) => {
-      return (
-        <div key={i}>
-          <h1>{user.name}</h1>
-          <img src={user.image} />
-        </div>
-      );
-    })}
+    <Counter />
   </>
 );
